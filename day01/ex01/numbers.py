11e5bcb4bb914a7ded5e5cbff7514e9202817e46
Sayed am <115349743+samjaabo@users.noreply.github.com>
2024-04-18 21:24:51 +0100

    Update numbers.py

--- day01/ex01/numbers.py
+++ day01/ex01/numbers.py
@@ -2,6 +2,7 @@ def read_file(file_name):
     a = open(file_name)
     print(a.read().replace(',', '\n'))
     # print(*a.read().split(','), sep = '\n')
+    a.close()
 
 if __name__ == '__main__':
     read_file('numbers.txt')
